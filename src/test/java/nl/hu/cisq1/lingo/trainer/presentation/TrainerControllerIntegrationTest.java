8fb91aec02201ea842d82b3b15f5bb72edc96af4
RaymonvdBrink <57092807+RaymonvdBrink@users.noreply.github.com>
2021-03-31 15:26:52 +0200

    domeinmodel deelopdracht 3? WIP 5? + 6 klaar fix?

--- src/test/java/nl/hu/cisq1/lingo/trainer/presentation/TrainerControllerIntegrationTest.java
+++ src/test/java/nl/hu/cisq1/lingo/trainer/presentation/TrainerControllerIntegrationTest.java
@@ -1,10 +1,12 @@
 package nl.hu.cisq1.lingo.trainer.presentation;
 
+import nl.hu.cisq1.lingo.CiTestConfiguration;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.ResultMatcher;
@@ -16,6 +18,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@Import(CiTestConfiguration.class)
 class TrainerControllerIntegrationTest {
 
     @Autowired
